28338050f0a3ac678aac566e2f3922188d26b2c1
NENT <62681122+Nentevo@users.noreply.github.com>
2021-01-14 11:05:10 +0800

    Create 1007.cpp

---
+++ 1007.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>//sqrt需要math库函数
+
+using namespace std;
+
+//判断数字n是否为素数，可以通过遍历2到n-1，看n是否能被他们整除
+//或者，令k=sqrt(n),只需判断n是否能被2到k整除即可。因为对任意合数n有a*b=n，若a<=k，则必有b>=k
+
+int main()
+{
+	int n,i,j,k;
+	int flag=0;//记录满足间距为2的素数对个数
+	int pre=2;//指代前一个素数，从第一个素数2开始
+	cin>>n;
+  
+	for(i=2;i<=n;i++)//不要从1开始，因为任何数都能被1整除
+	{
+		k=(int)sqrt((double)i);//sqrt()的参数为double类型，需要强制转换类型
+		for(j=2;j<=k;j++)
+		{	
+			if(i%j == 0)
+				break;
+		}
+		if(j>k)//遇到
+		{
+			if(i-pre == 2)
+			{
+				flag++;
+			}
+			pre=i;
+		}	
+	}
+  
+	cout<<flag<<endl;
+	return 0;
+}
